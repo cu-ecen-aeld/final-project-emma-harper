eea4739f44b6f3d5cd0dc77b8e721e10706c0f58
Emma Harper <emmaharps369@gmail.com>
2022-04-19 17:30:57 -0500

    add daemonizing

--- sensehatlive/sensehatlive.py
+++ sensehatlive/sensehatlive.py
@@ -4,16 +4,12 @@
  @Programmer(s)
     Kenneth A. Jones II
     kejo1166@colorado.edu
-
  @Company
     University of Colorado Boulder
-
 @Description
     Main application entry script
-
 @Reference
     None
-
 """
 
 import os
@@ -21,56 +17,91 @@ import sys
 
 # Ensure lib added to path, before any other imports
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'lib/'))
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'sensehatlive/'))
 
+import sensehatlive
 import argparse
 import signal
 import sys
 import time
 
+import sensehatlive.rabbitmq as mq
 import sensehatlive.log.logger as logger
-from sense_hat import SenseHat
 
+sh = None
 
-def sig_handler(signum=None, frame=None):
-    ''' Signal handler
-
-    :param signum: Signal responsible for call back
-    :return:
-    '''
-    if signum is not None:
-        logger.info("Signal %i caught, exiting...", signum)
-
-    # Clear exit
-    sys.exit()
-
+# Register signals
+signal.signal(signal.SIGINT, sensehatlive.sig_handler)
+signal.signal(signal.SIGTERM, sensehatlive.sig_handler)
 
 def main(args):
     ''' Main function
-
     :param args: Command line arguments
     :return:
     '''
+    global sh
 
-    # Register signals
-    signal.signal(signal.SIGINT, sig_handler)
-    signal.signal(signal.SIGTERM, sig_handler)
+    sensehatlive.VERBOSE = args.verbose
+    sensehatlive.QUIET = args.quiet
 
     # Initialize the logger
-    logger.initLogger(console=not args.quiet, log_dir=False, verbose=args.verbose)
+    logger.initLogger(console=not sensehatlive.QUIET, log_dir=os.path.dirname(__file__), verbose=sensehatlive.VERBOSE)
 
-    rc = 0
-    try:
-        logger.info('Sense Hat Live!: Producer')
-        #TODO: Create instance of sense hat manager
-        while True:
-            #TODO: Add code for main thread task
-            time.sleep(1)
+    if args.daemon:
+        logger.warning('Overriding quiet option when daemonizing')
+        sensehatlive.DAEMON = True
+        sensehatlive.QUIET = False
 
-    except Exception as e:
-        logger.error('Exception caught: ' + str(e))
+    if args.pidfile:
+        sensehatlive.PIDFILE = str(args.pidfile)
 
-    sys.exit(rc)
+        # If the pidfile already exists, sense hat live may still be running, so exit
+        if os.path.exists(sensehatlive.PIDFILE):
+            msg = "PID file '{}' already exists. Removing ...".format(sensehatlive.PIDFILE)
+            logger.warning(msg)
+            os.remove(sensehatlive.PIDFILE)
 
+        # The pidfile is only useful in daemon mode, make sure we can write the file properly
+        if sensehatlive.DAEMON:
+            sensehatlive.CREATEPID = True
+
+            try:
+                with open(sensehatlive.PIDFILE, 'w') as fp:
+                    fp.write("pid\n")
+            except IOError as e:
+                msg ="Unable to write PID file: {}".format(e)
+                logger.warning(msg)
+                raise SystemExit(msg)
+
+        else:
+            logger.warn("Not running in daemon mode. PID file creation disabled.")
+
+    if sensehatlive.DAEMON:
+        sensehatlive.daemonize()
+
+    logger.info('Sense Hat Live!: Consumer')
+    sh = mq.RabbitMQConsumer(queue='samples',on_msg_callback=None)
+    sensehatlive.insert_thread(sh)
+
+    # Start all threads
+    sensehatlive.start_threads()
+
+    while True:
+        if not sensehatlive.SIGNAL:
+            try:
+                time.sleep(1)
+
+            except KeyboardInterrupt:
+                sensehatlive.SIGNAL = 'shutdown'
+                sensehatlive.stop_threads()
+
+        else:
+            logger.info('Received signal: {}', sensehatlive.SIGNAL)
+            sensehatlive.stop_threads()
+
+            if sensehatlive.SIGNAL == 'shutdown':
+                sensehatlive.shutdown()
+            sensehatlive.SIGNAL = None
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Sense HAT Live! - Producer')
@@ -78,5 +109,9 @@ if __name__ == '__main__':
                         help='Increase console logging verbosity')
     parser.add_argument('-q', '--quiet', action='store_true',
                         help='Turn off console logging')
+    parser.add_argument('-d', '--daemon', action='store_true',
+                        help='Run as a daemon')
+    parser.add_argument('--pidfile',
+                        help='Create a pid file (only relevant when running as a daemon)')
     args = parser.parse_args()
     main(args)
